4006e06c3548d0ca65fbf66eb7a32ee345d5acf9
Rafael Lima <rafaa99@gmail.com>
2022-06-27 10:50:02 -0300

    learning about ownership & borrowing. testing unwarp and expect methods

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,43 @@ struct BankAccount {
     verified: bool,
 }
 
+fn print_balance(account: &BankAccount) {
+    println!("{:?}", account.balance);
+}
+
+fn print_verified(account: &BankAccount) {
+    println!("{:?}", account.verified);
+}
+
+fn is_verified(account: &BankAccount) -> Result<bool, bool> {
+    return match account.verified {
+        true => Ok(true),
+        false => Err(false),
+    };
+}
+
 fn main() {
     let my_account = BankAccount {
         balance: 50000,
         verified: true,
     };
+    let my_account2 = BankAccount {
+        balance: 50000,
+        verified: false,
+    };
 
-    println!("{:?}", my_account.balance);
-    println!("{:?}", my_account.verified);
+    let verification_status = is_verified(&my_account).unwrap();
+
+    // this will make Rust panic and throw an error
+    // let verification_status2 = is_verified(&my_account2).expect("<Write and error message here>");
+
+    // println!("{:?}", my_account.balance);
+    // println!("{:?}", my_account.verified);
+
+    print_balance(&my_account);
+    print_verified(&my_account);
+    println!("{:?}", verification_status);
+
+    // the next line will print the error
+    // println!("{:?}", verification_status2);
 }
